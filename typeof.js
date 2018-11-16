543facd2a8d7346109de7be3b5425350d2a389bc
nabil <nabilalcha58@gmail.com>
2018-11-17 06:02:31 +0700

    Belajar Mengecek Tipe Data javascript

---
+++ typeof.js
@@ -0,0 +1,13 @@
+function checkTipeData() {
+    // var data = "Nama"; // tipe string
+    // var data = 2000; // tipe number 
+    // var data = [1,2,3,4,5]; // array bentuk nya object
+    var data = {     // bisa juga seperti ini sama object juga
+        nama: "Nabil",
+        umur: 18
+    }
+    console.log(typeof(data))
+    // typeof(data) // cara untuk mengecek tipe data
+}
+
+checkTipeData()
