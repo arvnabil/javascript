9e4faba332df8f500396acdafdf25487f8836b53
nabil <nabilalcha58@gmail.com>
2018-11-17 05:53:39 +0700

    Belajar Objects dan Array dalam Objects  javascript

---
+++ objects.js
@@ -0,0 +1,15 @@
+function panggilObjects() {
+  var mobil = {
+    type: 'sedan',
+    harga: 2000000000,
+    warna: 'putih',
+    tahun: [2000,2001,2002,2003]
+    //array   0    1    2    3
+  }
+// console.log(mobil)
+// console.log(mobil.harga);
+console.log(mobil.tahun[2]);
+}
+
+
+panggilObjects()
