e70221a4204d797e863ed0528521b0a00d6b91e1
nabil <nabilalcha58@gmail.com>
2018-11-17 05:37:58 +0700

    Belajar Switch Statement

---
+++ switch.js
@@ -0,0 +1,20 @@
+function testingSwitch() {
+    var  nilai = 10;
+    switch (nilai) { //Switch Untuk menghandling nilai variabel
+      case 10:
+        console.log("sangat baik");
+        break;
+      case 8:
+        console.log("baik");
+        break;
+      case 6:
+        console.log("cukup");
+        break;
+        default:
+        console.log("lainnya");
+    }
+
+}
+
+testingSwitch()
+// maka akan mengasilkan "Sangat Baik" Karena variabel nilai nya = 10 
