ec568c2a268fc70cc0abe8d1ee8c24dde108529a
nabil <nabilalcha58@gmail.com>
2018-11-17 05:45:22 +0700

    Belajar Array javascript

---
+++ array.js
@@ -0,0 +1,13 @@
+function panggilArray() {
+  var data = [1,2,3,4,5,9]
+  var nama = ["abi","abi2","abi3"]
+
+  console.log(data.length);
+  console.log(nama.length); //cara menghitung jumlah yang ada di dalam array
+
+  console.log(nama[0]); // 0 adalah data array
+  console.log(nama[1]);
+  console.log(nama[2]);
+}
+
+panggilArray()
