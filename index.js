4b05184466e175384db5e0302ab6b6203ff2adef
nabil <nabilalcha58@gmail.com>
2018-11-17 04:40:40 +0700

    belajar-javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+var message = "Hello Coding";
+let msg1 = "hallo";
+
+const msg2 = "coding";
+
+if (true) {
+  var message = "Hello ke 2";
+  let msg1 = "hallo bro";
+  console.log("testing", msg1);
+}
+
+console.log(message)
+console.log(msg1)
