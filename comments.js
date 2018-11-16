987f69f40d0a6bd7bbaa8c2576559632cccfbda7
nabil <nabilalcha58@gmail.com>
2018-11-17 06:20:00 +0700

    Belajar Membuat Komentar di  javascript

---
+++ comments.js
@@ -0,0 +1,11 @@
+function panggilComments()
+{
+    console.log('satu')
+        // console.log('dua')  // komen menggunakan 2 garis miring di depan
+        //     console.log('tiga') // bisa menggunakan ctrl + / secara bersamaan 
+        // untuk mengembalikan tekan ctrl + / secara bersamaan juga
+// bisa juga seperti ini 
+/*Ini Komentar Saya*/
+    }
+
+panggilComments()
