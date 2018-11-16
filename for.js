d5fdb0f3d186834062cc63e85ceec1a10cd6aef6
nabil <nabilalcha58@gmail.com>
2018-11-17 05:41:09 +0700

    Belajar Perulangan FOR javascript

---
+++ for.js
@@ -0,0 +1,7 @@
+function perulangan() {
+  for (var i = 1; i < 1000; i++) {   //perulangan dari 1 sampe 999
+    console.log("jalan :", i)
+    }
+}
+perulangan()
+// maka akan mengulang sampai angka 999
