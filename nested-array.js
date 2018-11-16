f6963ae50fdafaa9b826dee257c6262faf9e9ea2
nabil <nabilalcha58@gmail.com>
2018-11-17 05:50:56 +0700

    Belajar Nested-Array javascript

---
+++ nested-array.js
@@ -0,0 +1,13 @@
+function panggilNestedArray() {
+  var dataNama = [["Mahasiswa R1X","Mahasiswa R1B","Mahasiswa R1C"],
+                  ["Nabil","Ayyub","Angga"],
+                  ["Icha","Ncek","nandito"]]
+ console.log(dataNama.length);
+ console.log(dataNama[0]); 
+ console.log(dataNama[1]); // memanggil Nama yang ada di array 1
+}
+
+panggilNestedArray()
+// maka hasil nya adalah 
+// [ 'Mahasiswa R1X', 'Mahasiswa R1B', 'Mahasiswa R1C' ]
+// [ 'Nabil', 'Ayyub', 'Angga' ]
