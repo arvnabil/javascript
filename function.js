0f67ca5654b8f6dc42182bb8d51b1dab0a9ca1e3
nabil <nabilalcha58@gmail.com>
2018-11-17 05:26:12 +0700

    Belajar Function

---
+++ function.js
@@ -0,0 +1,17 @@
+function JalanSatu() {
+  console.log("Jalan satu");
+
+}
+
+const JalanDua = function () {
+    console.log("Jalan Dua");
+}
+
+JalanSatu()
+JalanDua()
+
+const JalanTiga = ()=> {
+  console.log("Jalan Tiga");
+}
+
+JalanTiga()
