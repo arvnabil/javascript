a8fffa8ed31cefe8381b1afc39717d27cdac9e03
nabil <nabilalcha58@gmail.com>
2018-11-17 05:33:17 +0700

    Belajar If dan Else

---
+++ if-else.js
@@ -0,0 +1,15 @@
+function jalansatu() {
+  var nilai = 2;
+  // if (nilai === 3) {
+  //   console.log("benar")
+  // }else if (nilai === 2) {           // yang dipakai yang ini aja
+  //   console.log("Dua")
+  // }else {
+  //   console.log("salah")
+  // }
+  if (nilai===2)console.log("OK")
+  else console.log ("bukan")
+}
+
+jalansatu()
+// akan menghasil kan OK jika sama dengan 2
