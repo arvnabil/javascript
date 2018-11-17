98e42783d933b03691ad003e599a0be32f4e5867
nabil <nabilalcha58@gmail.com>
2018-11-17 07:14:45 +0700

    Belajar IF dan ELSE Berdasarkan Tipe Data

---
+++ tipedata.js
@@ -0,0 +1,11 @@
+function tipeData(){ 
+    var data = 'jakarta'; // tipe data string
+    if(data === 'jakarta'){ 
+        return data
+    } else {
+        return 'gagal'
+    }
+    // hasil nya true karna data === jakarta isinya sesuai dengan isi variabel 
+}
+
+console.log(tipeData() )
