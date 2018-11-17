f64611412092c25b86ae5e5e6af5d3aa42b05adf
nabil <nabilalcha58@gmail.com>
2018-11-17 07:00:49 +0700

    Belajar Operator di javascript

---
+++ operators.js
@@ -0,0 +1,13 @@
+function panggilOperator()
+{
+    var a = 4;
+    var b = 3;
+
+    return a+b // untuk mengakhiri proses fungtion
+    // return a*b 
+    // return a-b 
+    // return a/b 
+}
+
+console.log(panggilOperator() )
+// cara memanggil dengan menggunakan console
