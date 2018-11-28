90fdfa15d5384702bf5f79d1f11f9da48d7f0696
arvnabil <nabilalcha58@gmail.com>
2018-11-28 17:48:50 +0700

    Belajar mengenal perintah Reverse

---
+++ array-reverse.js
@@ -0,0 +1,14 @@
+function panggilReverse() {
+    var kota = ["Jakarta", "Bandung", "bali", "Malang"];
+    console.log(kota);
+    
+    var result = kota.reverse()
+    return result
+}
+
+console.log(panggilReverse());
+
+// reverse digunakan untuk membalikkan isi array dari jakarta sampai malang dibalik menjadi malang sampai jakarta
+// hasil nya menjadi sperti ini
+// [ 'Jakarta', 'Bandung', 'bali', 'Malang' ]
+// [ 'Malang', 'bali', 'Bandung', 'Jakarta' ]
