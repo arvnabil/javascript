432f977d009988f1ca71ed26c58fb070df5d8932
arvnabil <nabilalcha58@gmail.com>
2018-11-28 15:36:28 +0700

    Belajar menggunakan perintah IndexOf

---
+++ array-indexof.js
@@ -0,0 +1,12 @@
+function panggilIndexof(){
+    var kota = ['jakarta', 'bandung', 'malang', 'makasar', 'medan'];
+
+    return kota.indexOf("malang")
+
+}
+console.log(panggilIndexof());
+
+// indexof digunakan unruk mengetahui dimana posisi suatu array 
+// contoh  kota.indexOf("malang") maka yang akan tampil adalah
+// 2  (malang ini berada di dalam index ke 2)
+// kenapa 2 ? karena array dimulai dari angka 0
