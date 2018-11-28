845fdd289b792cf99645b8d46023ae4695c0513c
arvnabil <nabilalcha58@gmail.com>
2018-11-28 15:55:15 +0700

    Belajar mengenal perintah Concat

---
+++ array-concat.js
@@ -0,0 +1,12 @@
+function panggilConcat(){
+    var kota = ["jakarta", "bandung", "malang", "makasar", "medan"];
+    var Kabupaten = ["Lampung timur", "jombang", "malang"];
+
+    var hasil = kota.concat(Kabupaten)
+
+    return hasil
+}
+
+console.log(panggilConcat());
+
+// concat digunakan untuk menggabungkan array dengan array
