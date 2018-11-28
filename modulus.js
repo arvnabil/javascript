18b037284bcbf1b48726a5c443a647c6a090f8f6
arvnabil <nabilalcha58@gmail.com>
2018-11-28 17:59:41 +0700

    Belajar menggunakan Operator Modulus

---
+++ modulus.js
@@ -0,0 +1,12 @@
+function panggilModulus() {
+    var data1 = 5;
+    var data2 = 2;
+    return data1 % data2;
+}
+
+
+console.log(panggilModulus());
+
+// Operator Modulus adalah sebuah operasi yang menghasilkan sisa pembagian dari suatu bilangan (terhadap bilangan pembaginya). 
+// Di javascript, modulus dilambangkan dengan simbol persen (%)
+// maka hasil diatas adalah 1 karna 5 dibagi 2 sisanya 1
