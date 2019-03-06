8c5624486c8f319c5427e0725bce7cea503e027c
arvnabil <nabilalcha58@gmail.com>
2019-03-06 08:41:52 +0700

    Belajar Membuat forEach

---
+++ foreach.js
@@ -0,0 +1,18 @@
+function panggilforEach() {
+    var data = ['a','b','c','o','e'];
+    data.forEach(function (item, index, array) {
+        console.log(item);
+        console.log(index);
+        console.log(array);
+        
+        
+        
+    })
+
+}
+
+panggilforEach();
+
+// item untuk menampilkan isi data aray sesuai index nya
+// index untuk menampilkan sesuai index array nya
+// array untuk menampilkan seluruh data didalam array
